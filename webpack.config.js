6626028848153fdb35cae3088a59871ef6c0b00f
carlosescom <carlos.escom.ipn@gmail.com>
2018-12-02 01:06:56 -0600

    Also corrected indentation, it allows proper line by line comparison.

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,35 @@ const path = require('path');
 
 process.env.BABEL_ENV = 'production';
 
-    module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'drizzle-react-components.js',
-        library: 'drizzle-react-components',
-        libraryTarget: 'umd',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        loaders: [{
-            test: /\.(js)$/,
-            include: path.resolve(__dirname, 'src'),
-            use: [{
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015','react']
-                }/* ,
-                options: {
-                    presets: ['react'],
-                    plugins: [require('babel-plugin-transform-class-properties'), require('babel-plugin-transform-object-rest-spread')]
-                } */
-            }]
-        }]
-    },
-    externals: [
-        'drizzle',
-        'drizzle-react',
-        'prop-types',
-        'react',
-        'redux'
-    ]
+  module.exports = {
+  entry: './src/index.js',
+  output: {
+      filename: 'drizzle-react-components.js',
+      library: 'drizzle-react-components',
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+      loaders: [{
+          test: /\.(js)$/,
+          include: path.resolve(__dirname, 'src'),
+          use: [{
+              loader: 'babel-loader',
+              query: {
+                  presets: ['es2015','react']
+              }/* ,
+              options: {
+                  presets: ['react'],
+                  plugins: [require('babel-plugin-transform-class-properties'), require('babel-plugin-transform-object-rest-spread')]
+              } */
+          }]
+      }]
+  },
+  externals: [
+      'drizzle',
+      'drizzle-react',
+      'prop-types',
+      'react',
+      'redux'
+  ]
 };
